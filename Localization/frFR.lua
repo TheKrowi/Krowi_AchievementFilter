be96c9151e05b6beb2798d6ff6fc3b5a7d210598
Krowi <Krowi@Home>
2022-11-06 18:21:34 +0100

    added missing french localisation #64

--- Localization/frFR.lua
+++ Localization/frFR.lua
@@ -1,4 +1,4 @@
--- [[ Exported at 2022-11-05 23-04-56 ]] --
+-- [[ Exported at 2022-11-06 18-20-04 ]] --
 -- [[ This code is automatically generated as an export from ]] --
 -- [[ an SQLite database and is not meant for manual edit. ]] --
 
@@ -35,4 +35,41 @@ L["Tabs"] = "Onglets";
 L["Special Categories"] = "Catégorie Spéciale";
 L["Current Zone"] = "Zone Actuelle";
 L["Selected Zone"] = "Zone Sélectionnée";
+L["Bur's Mount Collection"] = "Collection de montures";
+L["Covenant Assaults"] = "Assauts de congrégations";
+L["Faction Assaults"] = "Assauts de faction";
+L["Gifts"] = "Cadeaux";
+L["Glory"] = "Gloire";
+L["Holidays"] = "Fêtes saisonnières";
+L["Legion Assaults"] = "Assauts de la Légion";
+L["N'Zoth Assaults"] = "Assauts de N'Zoth";
+L["Other"] = "Autre";
+L["Pirates' Day"] = "Jour des pirates";
+L["The Entitled"] = "Titres pour";
+L["Vendor"] = "Marchands";
+L["WoW's Anniversary"] = "Anniversaire de WoW";
+L["Fated Raids"] = "Raids fatidiques";
+L["Ta's Pet Collection"] = "Collection de mascottes";
+L["Going away with"] = "Temporaires";
+L["Dragonflight Pre-patch"] = "Prépatch";
+L["Day of the Dead"] = "Jour des morts";
+L["Assault on Azsuna"] = "Azsuna";
+L["Assault on Highmountain"] = "Haut-Roc";
+L["Assault on Stormheim"] = "Tornheim";
+L["Assault on Val'sharah"] = "Val'sharah";
+L["Assault on Tiragarde Sound"] = "Rade de Tiragarde";
+L["Assault on Drustvar"] = "Drustvar";
+L["Assault on Stormsong Valley"] = "Vallée Chantorage";
+L["Assault on Nazmir"] = "Nazmir";
+L["Assault on Vol'dun"] = "Vol'dun";
+L["Assault on Zuldazar"] = "Zuldazar";
+L["Necrolord Assault"] = "Nécro-seigneurs";
+L["Venthyr Assault"] = "Venthyrs";
+L["Kyrian Assault"] = "Kyrians";
+L["Night Fae Assault"] = "Faë nocturnes";
+L["Not earned by:"] = "Non obtenu par:";
+L["Part of a chain"] = "Chaîne";
+L["Required for"] = "Requis pour";
+L["Other faction"] = "Faction adverse";
+L["Objectives progress"] = "Progression";
 
