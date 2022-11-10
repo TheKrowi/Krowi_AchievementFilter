52d55e294c9b1e41653f29d3c0a4fe0dbc915204
Krowi <Krowi@Home>
2022-11-11 00:09:02 +0100

    #59 added Draenor Cuisine

--- Data/ExportedTooltipDataWrathClassic.lua
+++ Data/ExportedTooltipDataWrathClassic.lua
@@ -1,4 +1,4 @@
--- [[ Exported at 2022-11-09 07-30-15 ]] --
+-- [[ Exported at 2022-11-11 00-08-05 ]] --
 -- [[ This code is automatically generated as an export from ]] --
 -- [[ an SQLite database and is not meant for manual edit. ]] --
 
