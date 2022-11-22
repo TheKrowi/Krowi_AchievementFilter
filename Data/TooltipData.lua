3d34c70a5fb380a0e5088e27f21ee8bf04ec1b73
Krowi <Krowi@Home>
2022-11-22 19:03:57 +0100

    tooltip debug data

--- Data/TooltipData.lua
+++ Data/TooltipData.lua
@@ -57,6 +57,9 @@ local function AddTooltipLine(tooltip, tooltipLine)
     tooltip:AddLine(icon .. " " .. string.trim(text:ReplaceVars{
         achievement = name
     }), color.R, color.G, color.B);
+    if addon.Diagnostics.DebugEnabled() then
+        tooltip:AddLine(tooltipLine.AchievementId .. " - " .. tooltipLine.CriteriaIndex);
+    end
 end
 
 local function ProcessUnit(tooltip, guid)
