7c42d6470829c46e89c5296dea01334ec3e70e32
Krowi <Krowi@Home>
2023-10-26 22:55:03 +0200

    localization chack is now silent

--- Localization/Localization.lua
+++ Localization/Localization.lua
@@ -6,14 +6,6 @@ function localization.SetColors(L)
     L["Not earned by:"] = L["Not earned by:"]:SetColorRed() .. " %s";
 end
 
-local gameLocale = GetLocale()
-if gameLocale == "enGB" then
-    gameLocale = "enUS"
-end
-function localization.GetLocale()
-    return gameLocale;
-end
-
 function localization.GetDefaultLocale()
-    return LibStub(addon.Libs.AceLocale):NewLocale(addonName, "enUS", true);
+    return LibStub(addon.Libs.AceLocale):NewLocale(addonName, "enUS", true, true);
 end
