eadc51047e70724897970d08b622decc5d10df70
Krowi <Krowi@Home>
2025-01-03 18:26:11 +0100

    added some extra tabs

--- NewOptionsMixin.lua
+++ NewOptionsMixin.lua
@@ -11,6 +11,7 @@ end
 KrowiAF_NewOptionsMixin = {};
 
 function KrowiAF_NewOptionsMixin:OnLoad()
+    -- self:SetTitle(addon.MetaData.Title);
     self:SetPortraitToAsset(134070);
 
     self.Tab1.Text:SetText(addon.L["General"]);
@@ -40,17 +41,50 @@ function KrowiAF_NewOptionsMixin:UpdateSelectedTab()
     KrowiAF_NewOptionsLayoutFrame:SetShown(selectedTab == 2);
 end
 
+-- local offset = 10;
+-- function KrowiAF_NewOptionsMixin:SetChildPoints(child)
+--     child:SetPoint("TOPLEFT", self.Inset, "TOPLEFT", offset, -offset);
+--     child:SetPoint("BOTTOMRIGHT", self.Inset, "BOTTOMRIGHT", -offset, offset);
+-- end
+
+KrowiAF_NewOptionsGeneralFrameTabButtonMixin = {};
+
+function KrowiAF_NewOptionsGeneralFrameTabButtonMixin:OnClick(button)
+    PanelTemplates_SetTab(self:GetParent(), self:GetID());
+
+    self:GetParent():UpdateSelectedTab();
+end
+
 KrowiAF_NewOptionsGeneralMixin = {};
 
 function KrowiAF_NewOptionsGeneralMixin:OnLoad()
+    -- self:GetParent():SetChildPoints(self);
     PanelTabButtonMixin.OnLoad(self);
+
+    self.Tab1.Text:SetText(addon.L["Info"]);
+    self.Tab2.Text:SetText(addon.L["Icon"]);
+    self.Tab3.Text:SetText(addon.L["Key Binding"]);
+    self.Tab4.Text:SetText(addon.L["Filters"]);
+    self.Tab5.Text:SetText(addon.L["Debug"]);
+
+    PanelTemplates_SetNumTabs(self, 5);
+	PanelTemplates_SetTab(self, 1);
+    PanelTemplates_AnchorTabs(self);
 end
 
 function KrowiAF_NewOptionsGeneralMixin:OnShow()
+    self:UpdateSelectedTab();
     -- self.Text:SetText("General");
     for k, v in next, self do
         print(k, v);
     end
+end
+
+function KrowiAF_NewOptionsGeneralMixin:UpdateSelectedTab()
+    local selectedTab = PanelTemplates_GetSelectedTab(self);
+
+    -- KrowiAF_NewOptionsGeneralFrame:SetShown(selectedTab == 1);
+    -- KrowiAF_NewOptionsLayoutFrame:SetShown(selectedTab == 2);
 end
 
 KrowiAF_NewOptionsLayoutMixin = {};
